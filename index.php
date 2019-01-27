aed4ba7268ea8c5f115d5e9e877de5e005756655
slhome2010 <slhome2010@mail.ru>
2019-01-27 19:21:36 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// Global define
+define('VERSION', 'v3.0.1');
+define('DIR_DOWNLOAD', '/downloads');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Languages
+@session_start();
+if(isset($_GET['lang'])) { 						// Выбранный язык отправлен скрипту через GET
+	$_SESSION['lang'] = trim(strip_tags($_GET['lang']));
+	$date = time() + 30*24*60*60;
+	setcookie('lang',trim(strip_tags($_GET['lang'])),$date);
+} else if (isset($_COOKIE['lang'])) { 				// Если язык уже выбран и сохранен в сессии отправляем его скрипту
+	$_SESSION['lang'] = $_COOKIE['lang'];
+} else { 									// Язык по умолчанию
+	$_SESSION['lang'] = 'en';
+}
+
+// Открываем текущий язык
+$CurentLang = addslashes($_SESSION['lang']);
+//if (is_file('/language/'.$CurentLang.'.php')) {
+//	require_once('/language/'.$CurentLang.'.php');
+//}
+header("Location: /".$CurentLang."/index.html");
+
+// Language Detection
+//$languages = array();
+?>
