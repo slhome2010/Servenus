2610740c9957369b1dffab831d2b24be97ad1e88
slhome2010 <slhome2010@mail.ru>
2020-11-15 18:32:03 +0600

    Clone in using

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-define ('VERSION', 'Currently v3.1.2');
-define ('VERSION_SHORT', 'v3.1.2');
-define ('LAST_VERSION', 'v3.1.2');
+define ('VERSION', 'Currently v3.1.3');
+define ('VERSION_SHORT', 'v3.1.3');
+define ('LAST_VERSION', 'v3.1.3');
